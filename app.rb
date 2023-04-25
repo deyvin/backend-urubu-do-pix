3d2161af88372036b8df8bdb7d9a4f2f3d03cc0a
Deyvid Nascimento <deyvin@gmail.com>
2023-04-25 19:15:39 -0300

    feat: add create user route

--- app.rb
+++ app.rb
@@ -5,4 +5,10 @@ require 'sinatra/activerecord'
 require 'dotenv'
 Dotenv.load
 
+require './app/adapters/controllers/users_controllers'
+
 set :database, { adapter: 'sqlite3', database: ENV['DB_PATH'] }
+
+post '/users' do
+  {result: UsersController.create(params) }.to_json
+end
