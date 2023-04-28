04cbad95f530cf8e8d99d98bbbb77a57f59c3e2b
Deyvid Nascimento <deyvin@gmail.com>
2023-04-28 08:18:16 -0300

    feature: add connection with database

--- config/environment.rb
+++ config/environment.rb
@@ -6,3 +6,6 @@ Bundler.require(:default)                   # load all the default gems
 Bundler.require(Sinatra::Base.environment)  # load all the environment specific gems
 require 'active_support/deprecation'
 require 'active_support/all'
+Dotenv.load
+
+set :database, { adapter: 'sqlite3', database: ENV['DB_PATH'] }
