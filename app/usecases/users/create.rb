6a3f9a299c105d2fd0b40735066fd47d98828a8c
Deyvid Nascimento <deyvin@gmail.com>
2023-04-28 08:19:05 -0300

    fix(User): add user name key

--- app/usecases/users/create.rb
+++ app/usecases/users/create.rb
@@ -12,8 +12,8 @@ module Users
     end
 
     def call
-      user = @user.new(@params[:name])
-      @user_repository.create(name: user.name)
+      user = @user.new(name: @params[:name])
+      @user_repository.create!(name: user.name)
     end
   end
 end
