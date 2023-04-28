37d515417e61edfec3a3ad8d80d7ea40ca0a4449
Deyvid Nascimento <deyvin@gmail.com>
2023-04-28 07:59:43 -0300

    test(User): Ensure user model works

---
+++ spec/app/model/user_spec.rb
@@ -0,0 +1,9 @@
+require "spec_helper"
+require './app/model/user'
+
+RSpec.describe User do
+  context 'with valid user' do
+    let(:user) { User.new(name: 'John') }
+    it { expect(user.name).to eq('John') }
+  end
+end
