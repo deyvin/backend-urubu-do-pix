4721bbfc694755a9f1e2fde951ccd76d127b59e3
Deyvid Nascimento <deyvin@gmail.com>
2023-05-02 19:16:38 -0300

    refactor: fix rubocop offenses

--- app/usecases/orders/create.rb
+++ app/usecases/orders/create.rb
@@ -1,5 +1,4 @@
 # frozen_string_literal: true
-require 'byebug'
 
 require './app/model/order'
 require './app/adapters/repositories/order_repository'
--- spec/app/model/user_spec.rb
+++ spec/app/model/user_spec.rb
@@ -1,9 +1,11 @@
+# frozen_string_literal: true
+
 require 'spec_helper'
 require './app/model/user'
 
 RSpec.describe User do
   context 'with valid user' do
-    let(:user) { User.new(name: 'John') }
+    let(:user) { described_class.new(name: 'John') }
 
     it { expect(user.name).to eq('John') }
   end
