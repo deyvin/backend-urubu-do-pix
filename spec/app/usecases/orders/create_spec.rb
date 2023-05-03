cb366128b8ebc72b58e71fe6d303fb942026ac91
Deyvid Nascimento <deyvin@gmail.com>
2023-05-03 19:22:16 -0300

    test(Orders): Ensure move user_id to route param works

--- spec/app/usecases/orders/create_spec.rb
+++ spec/app/usecases/orders/create_spec.rb
@@ -6,8 +6,13 @@ require './app/usecases/users/create'
 
 RSpec.describe Orders::Create do
   context 'with new order' do
-    let(:user) { Users::Create.new(params: { name: 'John' }).call }
-    let(:create_order) { described_class.new(params: { value: 200, user_id: user.id }) }
+    let(:user) do
+      Users::Create.new(params: { name: 'John' }).call
+    end
+    
+    let(:create_order) do
+      described_class.new(params: { value: 200 }).with_user(user.id)
+    end
 
     it { expect(create_order.call.class).to be(OrderRepository) }
     it { expect(create_order.call.user_id).to be(user.id) }
